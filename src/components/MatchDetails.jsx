0fb0c43e4f32d31c0bf6c01bd3315e3427e2ef09
lindenau-dominika <lindenau.dominika@gmail.com>
2023-12-23 14:36:24 +0100

    NIE PUSHOWAC, aim subpage for match details began

--- src/components/MatchDetails.jsx
+++ src/components/MatchDetails.jsx
@@ -18,11 +18,23 @@ export const MatchDetails = () => {
     const [Clutches, setClutches] = useState([]); //Clutches
     const [ctClutches, setCtClutches] = useState([]);
     const [ttClutches, setTtClutches] = useState([]);
+
     const [isUtility, setIsUtility] = useState(false);
     const [Utility, setUtility] = useState(false);
     const [ctUtility, setCtUtility] = useState(false);
     const [ttUtility, setTtUtility] = useState(false);
 
+    const [isTradesOK, setIsTradesOK] = useState(false);
+    const [TradesOK, setTradesOK] = useState(false);
+    const [ctTradesOK, setCtTradesOK] = useState(false);
+    const [ttTradesOK, setTtTradesOK] = useState();
+
+    const [isAim, setIsAim] = useState(false);
+    const [Aim, setAim] = useState();
+    const [ctAim, setCtAim] = useState();
+    const [ttAim, setTtAim] = useState();
+    const [Weapons, setWeapons] = useState();
+    const [hitGroup, setHitGroup] = useState();
 
     const generateGeneralData = (teamStats) => {
         return teamStats.map((user) => [
@@ -87,6 +99,23 @@ export const MatchDetails = () => {
         ]);
       }
 
+      const generateTradesOKData = (teamStats) => {
+        return teamStats.map((user) => [
+            {
+                image: user.avatar,
+                nickname: user.username,
+            },
+            user.null,
+            user.trade_kill_attempts,
+            user.trade_kill_success,
+            user.traded_death_attempts,
+            user.traded_death_success,
+            user.opening_attempts,
+            user.opening_success,
+            user.opening_traded,
+        ])
+      }
+
     const GenColNames = [ 'Username','Rating', 'Kills', 'Deaths', 'K/D Ratio', 'Total Damage', 'Hs kill %', 'Rounds Survived'];
     
     const ClutchColNames = ['username', '','_2k', '_3k', '_4k', '_5k',
@@ -95,7 +124,10 @@ export const MatchDetails = () => {
     const UtilityColNames = ['Username','', 'Enemies flashed', 'Flash assists', 'Friends flashed', 'Self-flashed', 'HE Damage', 'Molotov damage',
         'AVG Blind Time', 'AVG unused Util Value', 'HE\'s thrown', 'Decoys thrown', 'Flashes Thrown', 'Smokes Thrown', 'Molos Thrown']
     
-    const handleMatchDetailsGeneral = async () => {
+    const TradesColNames = ['Username', '', 'Trade Kills Attempts', 'Trade Kills Success', 'Traded Death Attempts', 'Traded Death Success',
+        'Opening attempts', 'Opening success', 'Openings traded'];
+
+    const handleGeneralStats = async () => {
         try {
             const response = await fetch(`/xd/matches/${matchId}/details/general`, {
                 method: 'GET',
@@ -121,7 +153,7 @@ export const MatchDetails = () => {
         }
     } 
 
-    const handleMatchDetailsClutches = async () => {
+    const handleClutchesStats = async () => {
         try {
             const response = await fetch(`/xd/matches/${matchId}/details/clutches`, {
             method: 'GET',
@@ -146,7 +178,7 @@ export const MatchDetails = () => {
     }
 }
 
-    const handleMatchDetailsUtility = async () => {
+    const handleUtilityStats = async () => {
         try {
             const response = await fetch(`/xd/matches/${matchId}/details/utility`, {
                 method: 'GET',
@@ -161,6 +193,54 @@ export const MatchDetails = () => {
                 setCtUtility(utility_ct);
                 setTtUtility(utility_tt);
                 console.log(utility)
+            } else {
+                console.error('Error during fetching utility')
+            }
+        } catch(error) {
+            console.error(error);
+        }
+    }
+
+    const handleTradesOKStats = async () => {
+        try {
+            const response = await fetch(`/xd/matches/${matchId}/details/openingkills`, {
+                method: 'GET',
+                headers: {'Content-Type' : 'application/json'},
+            });
+            if (response.ok) {
+                const data = await response.json();
+                const tradesok = data.match_details_openingkills_stats || [];
+                const tradesok_ct = data.match_details_openingkills_ct_stats || [];
+                const tradesok_tt = data.match_details_openingkills_tt_stats || [];
+                setTradesOK(tradesok);
+                setCtTradesOK(tradesok_ct);
+                setTtTradesOK(tradesok_tt);
+            } else {
+                console.error('Error during fetching trades')
+            }
+        } catch(error) {
+            console.error(error);
+        }
+    }
+
+    const handleAimStats = async () => {
+        try {
+            const response = await fetch(`/xd/matches/${matchId}/details/aim`, {
+                method: 'GET',
+                headers: {'Content-Type': 'application/json'},
+            });
+            if (response.ok) {
+                const data = await response.json();
+                const aim = data.match_details_aim_overall_stats || [];
+                const aim_ct = data.match_details_aim_ct_overall_stats || [];
+                const aim_tt = data.match_details_aim_tt_overall_stats || [];
+                const weapon_details = data.match_details_aim_weapon_details || [];
+                const hit_group = data.match_details_aim_weapon_hit_groups || [];
+                setAim(aim);
+                setCtAim(aim_ct);
+                setTtAim(aim_tt);
+                setWeapons(weapon_details);
+                setHitGroup(hit_group);
             }
         } catch(error) {
             console.error(error);
@@ -168,9 +248,10 @@ export const MatchDetails = () => {
     }
 
 useEffect(() => {
-    handleMatchDetailsClutches(); 
-    handleMatchDetailsGeneral();
-    handleMatchDetailsUtility();
+    handleClutchesStats(); 
+    handleGeneralStats();
+    handleUtilityStats();
+    handleTradesOKStats();
 }, [matchId]);
 
 if (isLoading) {
@@ -197,14 +278,22 @@ const t1CtUtility = generateUtilityData(ctUtility.filter(user => user.team === 1
 const t0TtUtility = generateUtilityData(ttUtility.filter(user => user.team === 0)) || [];
 const t1TtUtility = generateUtilityData(ttUtility.filter(user => user.team === 1)) || [];
 
+const t0Trades = generateTradesOKData(TradesOK.filter(user => user.team === 0)) || [];
+const t1Trades = generateTradesOKData(TradesOK.filter(user => user.team === 1)) || [];
+const t0CtTrades = generateTradesOKData(ctTradesOK.filter(user => user.team === 0)) || [];
+const t1CtTrades = generateTradesOKData(ctTradesOK.filter(user => user.team === 1)) || [];
+const t0TtTrades = generateTradesOKData(ttTradesOK.filter(user => user.team === 0)) || [];
+const t1TtTrades = generateTradesOKData(ttTradesOK.filter(user => user.team === 1)) || [];
+
+
 
     
     return (<>
     <div className='match-buttons-container'>
-        <button className={`side-buttons ${isGeneral ? 'ct-selected': ''}`} onClick={() => {setIsGeneral(true); setIsClutches(false); setIsUtility(false)}} type='button'>General</button>
-        <button className={`side-buttons ${isClutches ? 'ct-selected': ''}`} onClick={() => {setIsGeneral(false); setIsClutches(true); setIsUtility(false)}} type='button'>Clutches</button>
-        <button className={`side-buttons ${isUtility ? 'ct-selected': ''}`} onClick={() => {setIsGeneral(false); setIsClutches(false); setIsUtility(true)}} type='button'>Utility</button>
-        {/* <button className={`side-buttons ${isUtility ? 'ct-selected': ''}`} onClick={() => {setIsGeneral(false); setIsClutches(false); setIsUtility(true)}} type='button'>Other</button> */}
+        <button className={`side-buttons ${isGeneral ? 'ct-selected': ''}`} onClick={() => {setIsGeneral(true); setIsClutches(false); setIsUtility(false); setIsTradesOK(false)}} type='button'>General</button>
+        <button className={`side-buttons ${isClutches ? 'ct-selected': ''}`} onClick={() => {setIsGeneral(false); setIsClutches(true); setIsUtility(false); setIsTradesOK(false)}} type='button'>Clutches</button>
+        <button className={`side-buttons ${isUtility ? 'ct-selected': ''}`} onClick={() => {setIsGeneral(false); setIsClutches(false); setIsUtility(true); setIsTradesOK(false)}} type='button'>Utility</button>
+        <button className={`side-buttons ${isTradesOK ? 'ct-selected': ''}`} onClick={() => {setIsGeneral(false); setIsClutches(false); setIsUtility(false); setIsTradesOK(true)}} type='button'>Trades & ok</button>
         {/* <button className={`side-buttons ${isClutches ? 'ct-selected': ''}`} onClick={() => {isGeneral(false); isClutches(true); isUtility(false)}} type='button'>Trades and Opening Kills TODO</button>
         <button className={`side-buttons ${isClutches ? 'ct-selected': ''}`} onClick={() => {isGeneral(false); isClutches(true); isUtility(false)}} type='button'>Aim TODO</button>
     <button className={`side-buttons ${isClutches ? 'ct-selected': ''}`} onClick={() => {isGeneral(false); isClutches(true); isUtility(false)}} type='button'>Other TODO</button> */}
@@ -214,6 +303,7 @@ const t1TtUtility = generateUtilityData(ttUtility.filter(user => user.team === 1
             <button className={`side-buttons ${CTsided ? 'ct-selected': ''}`} onClick={() => {setCTsided(true); setOverall(false); setTTsided(false)}} type='button'>Counter-terrorists Side</button>
             <button className={`side-buttons ${Overall ? 'oall-selected': ''}`} onClick={() => {setCTsided(false); setOverall(true); setTTsided(false)}} type='button'>Overall</button>
             <button className={`side-buttons ${TTsided ? 'terro-selected': ''}`} onClick={() => {setCTsided(false); setOverall(false); setTTsided(true);}} type='button'>Terrorists Side</button>
+            {isAim? (<button></button>) : null}
     </div>
         {isGeneral & Overall? (<>
             <div className='oall-container'>
@@ -295,9 +385,30 @@ const t1TtUtility = generateUtilityData(ttUtility.filter(user => user.team === 1
                 <TableTemplate tableData={t1TtUtility} colNames={UtilityColNames}/>
             </div></>
         ) : null}
-                
-
-
+            {isTradesOK & Overall ? (<>
+            <div className='oall-container'>
+                <TableTemplate tableData={t0Trades} colNames={TradesColNames}/>
+            </div>
+            <div className='oall-container'>
+                <TableTemplate tableData={t1Trades} colNames={TradesColNames}/>
+            </div></>
+        ) : null}
+                {isTradesOK & CTsided ? (<>
+            <div className='ct-container'>
+                <TableTemplate tableData={t0CtTrades} colNames={TradesColNames}/>
+            </div>
+            <div className='ct-container'>
+                <TableTemplate tableData={t1CtTrades} colNames={TradesColNames}/>
+            </div></>
+        ) : null}
+                {isTradesOK & TTsided ? (<>
+            <div className='terro-container'>
+                <TableTemplate tableData={t0TtTrades} colNames={TradesColNames}/>
+            </div>
+            <div className='terro-container'>
+                <TableTemplate tableData={t1TtTrades} colNames={TradesColNames}/>
+            </div></>
+        ) : null}
     </>
     )
 }
